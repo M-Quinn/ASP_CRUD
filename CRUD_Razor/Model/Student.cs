8671b6bcfca2d21506da5cdb53ad580775338947
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2025-01-18 03:48:17 -0500

    Created Model for Razor project

---
+++ CRUD_Razor/Model/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace CRUD_Razor.Model
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("First Name")]
+        public string FName { get; set; }
+        [DisplayName("Last Name")]
+        public string LName { get; set; }
+        [DisplayName("Grade Point Average")]
+        public float Gpa { get; set; }
+    }
+}
