8eae5ccaa1d56aa55a27ca88cba9867b4070a6d6
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2025-01-18 04:00:02 -0500

    Created migration for table creation
    EF Core table creation

---
+++ CRUD_Razor/Migrations/20250118085924_CreateTableRazorProject.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUD_Razor.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableRazorProject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students_Razor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gpa = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students_Razor", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students_Razor");
+        }
+    }
+}
