5e5f081b0d6109a67b917e10c1a1264a9c2383d3
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2025-01-18 03:53:34 -0500

    Created DbContext subclass

---
+++ CRUD_Razor/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CRUD_Razor.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_Razor.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+        }
+
+        public DbSet<Student> Students_Razor { get; set; }
+    }
+}
