16a2496181f4f7011a27d70aa7933260d933b9c0
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2025-01-18 04:01:13 -0500

    updated functionality to pull from database
    sets DbContext reference and pulls data from SQL table

--- CRUD_Razor/Pages/Index.cshtml.cs
+++ CRUD_Razor/Pages/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using CRUD_Razor.Data;
+using CRUD_Razor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,16 +7,20 @@ namespace CRUD_Razor.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public List<Student> Students { get; set; }
         private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _dbContext;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext dbContext)
         {
             _logger = logger;
+            _dbContext = dbContext;
         }
 
         public void OnGet()
         {
-
+            Students = _dbContext.Students_Razor.ToList();
         }
     }
 }
