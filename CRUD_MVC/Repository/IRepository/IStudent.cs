f7a80934d1cd230c13f44bfb0bce39a0f630f10c
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2025-01-20 09:50:02 -0500

    Created the sub versions

---
+++ CRUD_MVC/Repository/IRepository/IStudent.cs
@@ -0,0 +1,10 @@
+﻿using CRUD.Model.Models;
+
+namespace CRUD_MVC.Repository.IRepository
+{
+    public interface IStudent : IRepository<Student>
+    {
+        void Update(Student obj);
+        void Save();
+    }
+}
---
+++ CRUD_MVC/Repository/StudentRepository.cs
@@ -0,0 +1,26 @@
+﻿using CRUD_MVC.Repository.IRepository;
+using CRUD.DataAccess.Data;
+using CRUD.Model.Models;
+
+namespace CRUD_MVC.Repository
+{
+    public class StudentRepository: Repository<Student>, IStudent
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public StudentRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Update(Student obj)
+        {
+            _dbContext.StudentsMVC.Update(obj);
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
