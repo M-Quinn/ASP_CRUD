f87920073fd5e151dccb25610ec36f272be7223c
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2025-01-20 09:17:55 -0500

    Created the base interface and class
    Created the base interface and class for the repository pattern implementation

---
+++ CRUD_MVC/Repository/Repository.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq.Expressions;
+using CRUD_MVC.Repository.IRepository;
+using CRUD.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_MVC.Repository
+{
+    public class Repository<T>:IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _dbContext;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+
+            dbSet = _dbContext.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return dbSet.ToList();
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+    }
+}
